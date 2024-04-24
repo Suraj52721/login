daa98d5e070f5ea532adb98b56b18c2d4138d7a5
Suraj Singh <suraj.52721@gmail.com>
2024-04-24 18:33:27 +0530

    Add files via upload

---
+++ home1.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+if (isset($_SESSION["username"])) {
+    $username = $_SESSION["username"];
+    session_write_close();
+} else {
+    // since the username is not set in session, the user is not-logged-in
+    // he is trying to access this page unauthorized
+    // so let's clear all session variables and redirect him to index
+    session_unset();
+    session_write_close();
+    $url = "./index.php";
+    header("Location: $url");
+}
+
+?>
+<HTML>
+<HEAD>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<TITLE>Welcome</TITLE>
+<link href="assets/css/phppot-style.css" type="text/css"
+	rel="stylesheet" />
+<link href="assets/css/user-registration.css" type="text/css"
+	rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</HEAD>
+<BODY>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Univent</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="page-content">Welcome <?php echo $username;?></div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <button type="button" class="btn btn-dark"><a href="logout.php">Logout</a></button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="phppot-container">
+		<div class="page-content">Welcome <?php echo $username;?></div>
+</div>
+
+</BODY>
+</HTML>
---
+++ login.php
@@ -0,0 +1,103 @@
+<?php
+use Phppot\Member;
+
+if (! empty($_POST["login-btn"])) {
+    require_once __DIR__ . '/Model/Member.php';
+    $member = new Member();
+    $loginResult = $member->loginMember();
+}
+?>
+<HTML>
+<HEAD>
+<TITLE>Login</TITLE>
+<link href="assets/css/phppot-style.css" type="text/css"
+	rel="stylesheet" />
+<link href="assets/css/user-registration.css" type="text/css"
+	rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<script src="vendor/jquery/jquery-3.3.1.js" type="text/javascript"></script>
+</HEAD>
+<BODY>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="https://univent.live/">Univent</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="https://univent.live/">Home</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+	<div class="phppot-container">
+		<div class="sign-up-container">
+			<div class="login-signup">
+				<a href="user-registration.php">Sign up</a>
+			</div>
+			<div class="signup-align">
+				<form name="login" action="" method="post"
+					onsubmit="return loginValidation()">
+					<div class="signup-heading">Login</div>
+				<?php if(!empty($loginResult)){?>
+				<div class="error-msg"><?php echo $loginResult;?></div>
+				<?php }?>
+				<div class="row">
+						<div class="inline-block">
+							<div class="form-label">
+								Username<span class="required error" id="username-info"></span>
+							</div>
+							<input class="input-box-330" type="text" name="username"
+								id="username">
+						</div>
+					</div>
+					<div class="row">
+						<div class="inline-block">
+							<div class="form-label">
+								Password<span class="required error" id="login-password-info"></span>
+							</div>
+							<input class="input-box-330" type="password"
+								name="login-password" id="login-password">
+						</div>
+					</div>
+					<div class="row">
+						<span class="center"><input class="btn" type="submit" name="login-btn"
+							id="login-btn" value="Login"></span>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<script>
+function loginValidation() {
+	var valid = true;
+	$("#username").removeClass("error-field");
+	$("#password").removeClass("error-field");
+
+	var UserName = $("#username").val();
+	var Password = $('#login-password').val();
+
+	$("#username-info").html("").hide();
+
+	if (UserName.trim() == "") {
+		$("#username-info").html("required.").css("color", "#ee0000").show();
+		$("#username").addClass("error-field");
+		valid = false;
+	}
+	if (Password.trim() == "") {
+		$("#login-password-info").html("required.").css("color", "#ee0000").show();
+		$("#login-password").addClass("error-field");
+		valid = false;
+	}
+	if (valid == false) {
+		$('.error-field').first().focus();
+		valid = false;
+	}
+	return valid;
+}
+</script>
+</BODY>
+</HTML>
