89cd18ac2c5e691bf3ce056fe658f794e99673f6
Suraj Singh <suraj.52721@gmail.com>
2024-04-24 18:29:35 +0530

    Add files via upload

---
+++ mine/nav.php
@@ -0,0 +1,28 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Univent</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
+
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+          <li class="nav-item"><a href="services.php" class="nav-link">Services</a></li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="portfolio.php" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Portfolio</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+                <a class="dropdown-item" href="portfolio.php">Portfolio</a>
+                <a class="dropdown-item" href="portfolio-single.php">Portfolio Single</a>
+              </div>
+          </li>
+          <li class="nav-item"><a href="blog.php" class="nav-link">Case Studies</a></li>
+          <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+          <li class="nav-item cta"><a href="contact.php" class="nav-link"><span>Get in touch</span></a></li>
+          <li class="nav-item"><button type="button" class="btn btn-dark"><a href="logout.php">Logout</a></button></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
